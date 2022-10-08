f3a1847ae9ab886a71fd0d71f81cc8ceef75ad62
tuoyl <tuoyl@ihep.ac.cn>
2022-10-08 20:33:07 +0800

    update docs

--- tatpulsar/data/profile.py
+++ tatpulsar/data/profile.py
@@ -143,9 +143,9 @@ class Profile():
         method: int, optional
             The normalization method utilized, optional methods are {0, 1}
             method = 0 : :math:`N = (P - P_{min})/(P_{max} - P_{min})`
-                if background range are selected (`bkg_range` is not None)
-                :math:`N = (P - \\bar{B})/(P_{max} - \\bar{B})`
-                where :math:`\\bar{B}` is the mean level in `bkg_range`
+            if background range are selected (`bkg_range` is not None)
+            :math:`N = (P - \\bar{B})/(P_{max} - \\bar{B})`
+            where :math:`\\bar{B}` is the mean level in `bkg_range`
             method = 1 : :math:`N = (P-P_{min})/\\bar{P}`
 
         bkg_range: list, optional
@@ -203,38 +203,3 @@ def phihist(phi, nbins, **kwargs):
     profile_object = Profile(counts, **kwargs)
 
     return profile_object
-
-def resampling_profile(profile, sample_num=1, kind='poisson'):
-    '''
-    resampling the profile
-
-    Parameters
-    ----------
-    profile : array
-        The un-normalized profile
-
-    sample_num : int, optional
-        number of the resamplings for the profile, the default number is 1
-
-    kind : str, optional
-        The distribution of the profile, default is poisson.
-        ('poisson', 'gaussian') are refering to the poisson and gauss distribution
-
-    Returns
-    -------
-    resampled_profile : array or ndarray
-        if sample_num == 1, return a one dimensional array
-        if sample_num >1 , return a multi-dimensional array
-    '''
-    raw_profile = np.array(profile.tolist()*sample_num)
-    if sample_num <= 0:
-        raiseError("The number of sampling must a positive integer")
-
-    if kind == "poisson":
-        resampled_profile = np.random.poisson(raw_profile)
-    elif kind == "gaussian":
-        pass #TODO
-
-    resampled_profile = resampled_profile.reshape(int(len(resampled_profile)/len(profile)),
-            int(len(profile)))
-    return resampled_profile
