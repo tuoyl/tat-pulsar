52d9a11ddcd79d164d2c66d3840ab18e693fa3d6
tuoyl <tuoyl@ihep.ac.cn>
2022-11-16 11:23:27 +0800

    Indentation bug fixed

--- tatpulsar/pulse/binarycor.pyx
+++ tatpulsar/pulse/binarycor.pyx
@@ -54,44 +54,44 @@ cpdef double Cor(double time, double pbp_t0, \
     -------
     torb: float
         The correction value
-  '''
-  cdef double torb
-  cdef double orbph
-  cdef double t
-  cdef double dt
-  cdef double dt2
-  cdef double e
-  cdef double ep, dep, denom ;
-  cdef double alpha, beta, omega, sbe, cbe, q
-  cdef double SECDAY = 86400.
-  cdef double TWOPI = 6.283185307179586
+    '''
+    cdef double torb
+    cdef double orbph
+    cdef double t
+    cdef double dt
+    cdef double dt2
+    cdef double e
+    cdef double ep, dep, denom ;
+    cdef double alpha, beta, omega, sbe, cbe, q
+    cdef double SECDAY = 86400.
+    cdef double TWOPI = 6.283185307179586
 
-  e = pbp_e
-  t = SECDAY * (time - pbp_t0)
-  dt = t / pbp_pb
-  dt2 = 0.5 * dt * dt
-  orbph = dt - dt2 * pbp_pbdot
-  orbph -= int(orbph)
-  if orbph < 0.0:
-    orbph = orbph + 1.0
-  orbph = TWOPI * orbph
-  ep = orbph + e*sin(orbph)*(1 + e*cos(orbph))
-  denom = 1.0 - e*cos(ep)
+    e = pbp_e
+    t = SECDAY * (time - pbp_t0)
+    dt = t / pbp_pb
+    dt2 = 0.5 * dt * dt
+    orbph = dt - dt2 * pbp_pbdot
+    orbph -= int(orbph)
+    if orbph < 0.0:
+      orbph = orbph + 1.0
+    orbph = TWOPI * orbph
+    ep = orbph + e*sin(orbph)*(1 + e*cos(orbph))
+    denom = 1.0 - e*cos(ep)
 
-  dep = 1.0
-  while ( fabs(dep) > 1.0e-12 ):
-     dep = ( orbph - ( ep - e * sin(ep) ) ) / denom
-     ep += dep
+    dep = 1.0
+    while ( fabs(dep) > 1.0e-12 ):
+       dep = ( orbph - ( ep - e * sin(ep) ) ) / denom
+       ep += dep
 
-  omega = pbp_omega + pbp_omdot * t
-  alpha = pbp_a1sini * sin(omega)
-  beta = pbp_a1sini * cos(omega) * sqrt(1 - e * e)
-  sbe = sin(ep)
-  cbe = cos(ep)
-  q = alpha * (cbe - e) + (beta + pbp_gamma) * sbe
-  torb = -q + (TWOPI / pbp_pb) * q * (beta * cbe - alpha * sbe) / (1 - e * cbe)
+    omega = pbp_omega + pbp_omdot * t
+    alpha = pbp_a1sini * sin(omega)
+    beta = pbp_a1sini * cos(omega) * sqrt(1 - e * e)
+    sbe = sin(ep)
+    cbe = cos(ep)
+    q = alpha * (cbe - e) + (beta + pbp_gamma) * sbe
+    torb = -q + (TWOPI / pbp_pb) * q * (beta * cbe - alpha * sbe) / (1 - e * cbe)
 
-  return torb
+    return torb
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -99,43 +99,43 @@ cpdef double Cor(double time, double pbp_t0, \
 cpdef double Doppler(double time, double pbp_t0, \
                  double pbp_e, double pbp_pb, double pbp_pbdot, double pbp_omega, double pbp_omdot, \
                  double pbp_a1sini, double pbp_gamma):
-  '''
-  The Astrophysical Journal, 584:996–1007, 2003 February 20
-  nu_obs = nu_emit * Doppler_factor 
-  '''
-  cdef double torb
-  cdef double orbph
-  cdef double t
-  cdef double dt
-  cdef double dt2
-  cdef double e
-  cdef double ep, dep, denom ;
-  cdef double alpha, beta, omega, sbe, cbe, q
-  cdef double SECDAY = 86400.
-  cdef double TWOPI = 6.283185307179586
+    '''
+    The Astrophysical Journal, 584:996–1007, 2003 February 20
+    nu_obs = nu_emit * Doppler_factor
+    '''
+    cdef double torb
+    cdef double orbph
+    cdef double t
+    cdef double dt
+    cdef double dt2
+    cdef double e
+    cdef double ep, dep, denom ;
+    cdef double alpha, beta, omega, sbe, cbe, q
+    cdef double SECDAY = 86400.
+    cdef double TWOPI = 6.283185307179586
 
-  e = pbp_e
-  t = SECDAY * (time - pbp_t0)
-  dt = t / pbp_pb
-  dt2 = 0.5 * dt * dt
-  orbph = dt - dt2 * pbp_pbdot
-  orbph -= int(orbph)
-  if orbph < 0.0:
-    orbph = orbph + 1.0
-  orbph = TWOPI * orbph
-  ep = orbph + e*sin(orbph)*(1 + e*cos(orbph))
-  denom = 1.0 - e*cos(ep)
+    e = pbp_e
+    t = SECDAY * (time - pbp_t0)
+    dt = t / pbp_pb
+    dt2 = 0.5 * dt * dt
+    orbph = dt - dt2 * pbp_pbdot
+    orbph -= int(orbph)
+    if orbph < 0.0:
+      orbph = orbph + 1.0
+    orbph = TWOPI * orbph
+    ep = orbph + e*sin(orbph)*(1 + e*cos(orbph))
+    denom = 1.0 - e*cos(ep)
 
-  dep = 1.0
-  while ( fabs(dep) > 1.0e-12 ):
-     dep = ( orbph - ( ep - e * sin(ep) ) ) / denom
-     ep += dep
+    dep = 1.0
+    while ( fabs(dep) > 1.0e-12 ):
+       dep = ( orbph - ( ep - e * sin(ep) ) ) / denom
+       ep += dep
 
-  omega = pbp_omega + pbp_omdot * t
-  alpha = pbp_a1sini * sin(omega)
-  beta = pbp_a1sini * cos(omega) * sqrt(1 - e * e)
-  sbe = sin(ep)
-  cbe = cos(ep)
+    omega = pbp_omega + pbp_omdot * t
+    alpha = pbp_a1sini * sin(omega)
+    beta = pbp_a1sini * cos(omega) * sqrt(1 - e * e)
+    sbe = sin(ep)
+    cbe = cos(ep)
 
-  return 1. - (TWOPI / pbp_pb) *  (beta * cbe - alpha * sbe) / (1 - e * cbe)
+    return 1. - (TWOPI / pbp_pb) *  (beta * cbe - alpha * sbe) / (1 - e * cbe)
 
